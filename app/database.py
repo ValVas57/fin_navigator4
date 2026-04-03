afc6b4a88c0a2ff2df578da4c768c6d3b7373c9b
ValVas57 <166316241+ValVas57@users.noreply.github.com>
2026-04-03 11:15:42 +0400

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,13 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./data/finnavigator.db"
+# Используем переменную окружения с URL базы данных
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}  # <--- ЭТО ВАЖНО!
-)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
