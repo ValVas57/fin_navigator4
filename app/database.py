cd8ccd32bc80927fb761e7431efca4251312e25e
ValVas57 <166316241+ValVas57@users.noreply.github.com>
2026-04-03 14:25:43 +0400

    Update database.py

--- app/database.py
+++ app/database.py
@@ -3,9 +3,15 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+# Создаём папку data, если её нет
+os.makedirs("data", exist_ok=True)
 
-engine = create_engine(DATABASE_URL)
+SQLALCHEMY_DATABASE_URL = "sqlite:///./data/finnavigator.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
